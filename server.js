23a625be4377655e0efbb45a60ec2b4d4f57eaf4
cordz-del <raarongraham@gmail.com>
2025-02-22 04:54:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
 const { Configuration, OpenAIApi } = require('openai');
 
-dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
