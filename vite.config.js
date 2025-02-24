fa3d2c738bd7569a69ca53e78bc92f3d51948143
cordz-del <raarongraham@gmail.com>
2025-02-23 19:41:27 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Amie-Chatbot-Frontend/',
   plugins: [react()],
-  root: '.',
-  build: {
-    outDir: 'docs',
-    sourcemap: true,
-  },
   server: {
-    port: 3000,
-    open: true
+    port: 3000
   },
-  resolve: {
-    alias: {
-      '@': '/src'
-    },
-    extensions: ['.js', '.jsx'] // Add this line to handle both .js and .jsx files
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   }
 });
