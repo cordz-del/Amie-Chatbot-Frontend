c7a671db73014a089850b3f3dde7fe93a1eec92b
cordz-del <raarongraham@gmail.com>
2025-02-23 22:54:51 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Amie-Chatbot-Frontend/', // Add this line for GitHub Pages
   plugins: [react()],
   server: {
     port: 5173,
